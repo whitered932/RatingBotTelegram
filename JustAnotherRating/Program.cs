e43e07d20fbc3f279de9cdce1dddb29181712351
Alexander Chernykh <dev@whitered932.ru>
2025-01-09 14:04:45 +0500

    косметические правки

--- JustAnotherRating/Program.cs
+++ JustAnotherRating/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
@@ -85,14 +86,21 @@ class Program
             return;
         }
 
-        var stats1x1 = ratings.GetFormattedStats1x1();
-        var stats2x2 = ratings.GetFormattedStats2x2();
+        var stats1X1 = ratings.GetFormattedStats1X1();
+        var stats2X2 = ratings.GetFormattedStats2X2();
 
-        var message = "Рейтинг участников:\n";
-        message += !string.IsNullOrWhiteSpace(stats1x1) ? $"1x1:\n{stats1x1}\n\n" : "";
-        message += !string.IsNullOrWhiteSpace(stats2x2) ? $"2x2:\n{stats2x2}" : "";
+        var message = new StringBuilder("Рейтинг участников:\n");
+        if (!string.IsNullOrWhiteSpace(stats1X1))
+        {
+            message.Append($"1x1:\n{stats1X1}\n\n");
+        }
 
-        await BotClient.SendMessage(chatId, message, cancellationToken: cancellationToken);
+        if (!string.IsNullOrWhiteSpace(stats2X2))
+        {
+            message.Append($"2x2:\n{stats2X2}");
+        }
+
+        await BotClient.SendMessage(chatId, message.ToString(), cancellationToken: cancellationToken);
     }
 
     private static async Task ShowInfo(long chatId, CancellationToken cancellationToken)
@@ -109,20 +117,20 @@ class Program
     {
         var args = messageText.Split(' ');
 
-        if (args.Length == 4 && args[0].Equals("/game"))
+        switch (args.Length)
         {
-            await ProcessGame1X1(chatId, args[1], args[2], args[3], cancellationToken);
-        }
-        else if (args.Length == 7 && args[0].Equals("/game"))
-        {
-            await ProcessGame2X2(chatId, args[1], args[2], args[3], args[4], args[5], args[6], cancellationToken);
-        }
-        else
-        {
-            await BotClient.SendMessage(chatId, "Неверный формат команды. Используйте: \n" +
-                                                "/game @player1 @player2 @winner для 1x1\n" +
-                                                "/game @player1 @player2 @player3 @player4 @winner1 @winner2 для 2x2",
-                cancellationToken: cancellationToken);
+            case 4 when args[0].Equals("/game"):
+                await ProcessGame1X1(chatId, args[1], args[2], args[3], cancellationToken);
+                break;
+            case 7 when args[0].Equals("/game"):
+                await ProcessGame2X2(chatId, args[1], args[2], args[3], args[4], args[5], args[6], cancellationToken);
+                break;
+            default:
+                await BotClient.SendMessage(chatId, "Неверный формат команды. Используйте: \n" +
+                                                    "/game @player1 @player2 @winner для 1x1\n" +
+                                                    "/game @player1 @player2 @player3 @player4 @winner1 @winner2 для 2x2",
+                    cancellationToken: cancellationToken);
+                break;
         }
     }
 
@@ -201,102 +209,5 @@ class Program
             Console.WriteLine($"Ошибка загрузки данных: {ex.Message}");
             return new Dictionary<long, Ratings>();
         }
-    }
-}
-
-/// <summary>
-/// Класс Ratings отвечает за управление рейтингами пользователей.
-/// </summary>
-class Ratings
-{
-    private readonly Dictionary<string, int> _rating1x1 = new();
-    private readonly Dictionary<string, int> _rating2x2 = new();
-    private const double KFactor = 32.0;
-
-    public void InitializePlayer(string player)
-    {
-        if (!_rating1x1.ContainsKey(player)) _rating1x1[player] = 1500;
-        if (!_rating2x2.ContainsKey(player)) _rating2x2[player] = 1500;
-    }
-
-    public bool UpdateRating1X1(string player1, string player2, string winner)
-    {
-        if (winner != player1 && winner != player2)
-        {
-            return false;
-        }
-
-        var loser = winner == player1 ? player2 : player1;
-
-        var ratingWinner = _rating1x1[winner];
-        var ratingLoser = _rating1x1[loser];
-
-        double expectedWinner = 1.0 / (1.0 + Math.Pow(10, (ratingLoser - ratingWinner) / 400.0));
-        double expectedLoser = 1.0 / (1.0 + Math.Pow(10, (ratingWinner - ratingLoser) / 400.0));
-
-        _rating1x1[winner] += (int)(KFactor * (1 - expectedWinner));
-        _rating1x1[loser] += (int)(KFactor * (0 - expectedLoser));
-
-        return true;
-    }
-
-    public bool UpdateRating2X2(string player1, string player2, string player3, string player4, string winner1,
-        string winner2)
-    {
-        var team1 = new HashSet<string> { player1, player2 };
-        var team2 = new HashSet<string> { player3, player4 };
-
-        var winners = new HashSet<string> { winner1, winner2 };
-
-        if (winners.IsSubsetOf(team1))
-        {
-            UpdateTeamRatings(team1, team2);
-        }
-        else if (winners.IsSubsetOf(team2))
-        {
-            UpdateTeamRatings(team2, team1);
-        }
-        else
-        {
-            return false;
-        }
-
-        return true;
-    }
-
-    private void UpdateTeamRatings(HashSet<string> winningTeam, HashSet<string> losingTeam)
-    {
-        int winningRating = winningTeam.Sum(player => _rating2x2[player]);
-        int losingRating = losingTeam.Sum(player => _rating2x2[player]);
-
-        double expectedWinning = 1.0 / (1.0 + Math.Pow(10, (losingRating - winningRating) / 800.0));
-        double expectedLosing = 1.0 / (1.0 + Math.Pow(10, (winningRating - losingRating) / 800.0));
-
-        foreach (var player in winningTeam)
-        {
-            _rating2x2[player] += (int)(KFactor * (1 - expectedWinning));
-        }
-
-        foreach (var player in losingTeam)
-        {
-            _rating2x2[player] += (int)(KFactor * (0 - expectedLosing));
-        }
-    }
-
-    public string GetFormattedStats1x1()
-    {
-        return string.Join("\n", _rating1x1.OrderByDescending(r => r.Value)
-            .Select(r => $"{r.Key}: {r.Value}"));
-    }
-
-    public string GetFormattedStats2x2()
-    {
-        return string.Join("\n", _rating2x2.OrderByDescending(r => r.Value)
-            .Select(r => $"{r.Key}: {r.Value}"));
-    }
-
-    public bool IsEmpty()
-    {
-        return !_rating1x1.Any() && !_rating2x2.Any();
     }
 }
---
+++ JustAnotherRating/Ratings.cs
@@ -0,0 +1,95 @@
+﻿namespace JustAnotherRating;
+
+public class Ratings
+{
+    private readonly Dictionary<string, int> _rating1X1 = new();
+    private readonly Dictionary<string, int> _rating2X2 = new();
+    private const double KFactor = 32.0;
+
+    public void InitializePlayer(string player)
+    {
+        _rating1X1.TryAdd(player, 1500);
+        _rating2X2.TryAdd(player, 1500);
+    }
+
+    public bool UpdateRating1X1(string player1, string player2, string winner)
+    {
+        if (winner != player1 && winner != player2)
+        {
+            return false;
+        }
+
+        var loser = winner == player1 ? player2 : player1;
+
+        var ratingWinner = _rating1X1[winner];
+        var ratingLoser = _rating1X1[loser];
+
+        var expectedWinner = 1.0 / (1.0 + Math.Pow(10, (ratingLoser - ratingWinner) / 400.0));
+        var expectedLoser = 1.0 / (1.0 + Math.Pow(10, (ratingWinner - ratingLoser) / 400.0));
+
+        _rating1X1[winner] += (int)(KFactor * (1 - expectedWinner));
+        _rating1X1[loser] += (int)(KFactor * (0 - expectedLoser));
+
+        return true;
+    }
+
+    public bool UpdateRating2X2(string player1, string player2, string player3, string player4, string winner1,
+        string winner2)
+    {
+        var team1 = new HashSet<string> { player1, player2 };
+        var team2 = new HashSet<string> { player3, player4 };
+
+        var winners = new HashSet<string> { winner1, winner2 };
+
+        if (winners.IsSubsetOf(team1))
+        {
+            UpdateTeamRatings(team1, team2);
+        }
+        else if (winners.IsSubsetOf(team2))
+        {
+            UpdateTeamRatings(team2, team1);
+        }
+        else
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void UpdateTeamRatings(HashSet<string> winningTeam, HashSet<string> losingTeam)
+    {
+        var winningRating = winningTeam.Sum(player => _rating2X2[player]);
+        var losingRating = losingTeam.Sum(player => _rating2X2[player]);
+
+        var expectedWinning = 1.0 / (1.0 + Math.Pow(10, (losingRating - winningRating) / 800.0));
+        var expectedLosing = 1.0 / (1.0 + Math.Pow(10, (winningRating - losingRating) / 800.0));
+
+        foreach (var player in winningTeam)
+        {
+            _rating2X2[player] += (int)(KFactor * (1 - expectedWinning));
+        }
+
+        foreach (var player in losingTeam)
+        {
+            _rating2X2[player] += (int)(KFactor * (0 - expectedLosing));
+        }
+    }
+
+    public string GetFormattedStats1X1()
+    {
+        return string.Join("\n", _rating1X1.OrderByDescending(r => r.Value)
+            .Select(r => $"{r.Key}: {r.Value}"));
+    }
+
+    public string GetFormattedStats2X2()
+    {
+        return string.Join("\n", _rating2X2.OrderByDescending(r => r.Value)
+            .Select(r => $"{r.Key}: {r.Value}"));
+    }
+
+    public bool IsEmpty()
+    {
+        return _rating1X1.Count == 0 && _rating2X2.Count == 0;
+    }
+}
